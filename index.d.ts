1ea1171d16abfab1423b3feb92ffc5e2282fb1c2
afeiship <1290657123@qq.com>
2024-05-16 09:44:49 +0800

    feat: add util.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="./modules/data.d.ts" />
 /// <reference types="./modules/event.d.ts" />
 /// <reference types="./modules/lang.d.ts" />
+/// <reference types="./modules/util.d.ts" />
