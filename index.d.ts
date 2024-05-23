f08f12054f85c842696584318b7d029f13f5969d
afeiship <1290657123@qq.com>
2024-05-23 17:09:39 +0800

    feat: add stattus

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,5 @@
 /// <reference types="./modules/data.d.ts" />
 /// <reference types="./modules/event.d.ts" />
 /// <reference types="./modules/lang.d.ts" />
+/// <reference types="./modules/status.d.ts" />
 /// <reference types="./modules/util.d.ts" />
---
+++ modules/status.d.ts
@@ -0,0 +1,3 @@
+type Disabled = {
+  disabled?: boolean;
+};
